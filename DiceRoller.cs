b5c5ef5632b2ec2bb926ffc98364fad244a3add6
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2024-01-17 21:28:28 -0700

    Comments

--- DiceRoller.cs
+++ DiceRoller.cs
@@ -7,15 +7,20 @@ using System.Threading.Tasks;
 
 class DiceRoller
 {
+    // Use random class create an object to generate random numbers
     private Random rand = new Random();
-
     public int[] RollDice(int numRolls)
     {
-        int[] rollCounts = new int[13]; // 2 to 12
+        // Creates an array to store the number of times a number was rolled
+        // Only indices 2-12 are used
+        int[] rollCounts = new int[13]; 
 
+        // For loop the keeps a counter for each number rolled and stores it in the roll counts array
         for (int iCount = 0; iCount < numRolls; iCount++)
         {
+            // Generates two random number 1-6 to simulate two dice and adds them together
             int roll = rand.Next(1, 7) + rand.Next(1, 7);
+            // Adds to the counter for whatever number was rolled 
             rollCounts[roll]++;
         }
 
--- Program.cs
+++ Program.cs
@@ -3,26 +3,33 @@
 
 class Program
 {
+    // method to retrieve numRolls for DiceRoller class
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the dice throwing simulator!\n");
         Console.Write("How many dice rolls would you like to simulate? ");
 
         int numRolls = int.Parse(Console.ReadLine());
+        // Creates the object variable
         DiceRoller roller = new DiceRoller();
 
+        // Creates array that stores rolled numbers received from RollDice method
         int[] results = roller.RollDice(numRolls);
 
         Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         Console.WriteLine("Total number of rolls = " + numRolls + ".\n");
 
+        // for loop for all possible dice rolls
         for (int iCount = 2; iCount <= 12; iCount++)
         {
             Console.Write(iCount + ": ");
+            // Formula to calculate number of *s
             int percentage = (results[iCount] * 100) / numRolls;
+            // Prints *s based on variable percentage
             Console.WriteLine(new String('*', percentage));
         }
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
     }
 }
 
